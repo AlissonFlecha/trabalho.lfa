82917529fc6b11b131188a43ffa2e898d9213a80
Alisson Flecha <afcflecha@gmail.com>
2017-11-13 13:06:58 -0200

    1311

--- trabalho.c
+++ trabalho.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-#define MAX 100
-
-#define DIREITA 1
-#define ESQUERDA 0
-#define TRUE 1
-#define FALSE 0
-
-typedef struct 
-{
-	int num;
-	char leitura;
-	int direcao;
-	char escrita;
-	int qual_ir;
-}estado;
+#include "trabalho.h"
 
 
 
@@ -31,7 +12,8 @@ void LoadArquivo(char *fn, estado *Estados, int *tam, int *finais, int *tam_fina
 			return;
 		}
 
-	fscanf(f,"Q=%d%*c",tam); 				        //lendo a quantidade de estados, logo o tamanho do vetor de estados
+	fscanf(f,"Q=%d%*c",tam); 				                  //lendo a quantidade de estados e ignorando (não sei pq, mas é necessário ter um argumento)
+
 
 	int i ;
 	char aux;
@@ -39,19 +21,19 @@ void LoadArquivo(char *fn, estado *Estados, int *tam, int *finais, int *tam_fina
 
 	while(aux != '\n')
 	{
-		fscanf(f,"%c",&aux);			            //lendo a linhas q contem os finais
-		if(isdigit(aux)){				        	//pegando somente os numeros
-			finais[(*tam_final)] = aux - '0';       //transformando em int
+		fscanf(f,"%c",&aux);			                      //lendo a linhas q contem os finais
+		if(isdigit(aux)){				        	          //pegando somente os numeros
+			finais[(*tam_final)] = aux - '0';                 //transformando em int
 			(*tam_final)++;
 		}
 	}	
 
 	char *batata[100];
-	fscanf(f,"%[^\n]%*c",batata);
+	fscanf(f,"%[^\n]%*c",batata);                            //lendo o alfabeto e ignorando,
 
 
 	i = 0;
-	 while(!feof(f))
+	 while(!feof(f))                                         //começa as transições
 	{
 		char lado;
 
@@ -59,16 +41,13 @@ void LoadArquivo(char *fn, estado *Estados, int *tam, int *finais, int *tam_fina
 		fscanf(f,"(q%d",&Estados[i].num);
 
 		fscanf(f,",%c)",&aux);
-		Estados[i].leitura = aux ;                                //o char diretamente na struct
+		Estados[i].leitura = aux ;                            //o char diretamente na struct
+		fscanf(f,"=(q%d",&Estados[i].qual_ir);                //lendo o próximo estado
 
-
-
-		fscanf(f,"=(q%d",&Estados[i].qual_ir);                    //lendo o próximo estado
-
-		fscanf(f,",%c",&aux);                                     //lendo o q escreve
+		fscanf(f,",%c",&aux);                                 //lendo o q escreve
 		Estados[i].escrita = aux;
 
-		fscanf(f,",%c%*c%*c",&lado);                                 //lendo o lado
+		fscanf(f,",%c%*c%*c",&lado);                          //lendo o lado
 
 		if(lado == 'D')
 			Estados[i].direcao = DIREITA;
@@ -82,12 +61,12 @@ void LoadArquivo(char *fn, estado *Estados, int *tam, int *finais, int *tam_fina
 
 }
 
-
+//printa o vetor de estados
 void PrintaVetorEstado(estado *estados, int tam)
 {
 	for (int i = 0; i < tam; ++i)
 	{
-		printf("num: %d\n",estados[i].num);
+		printf("num: %d\n",estados[i].num); 
 		printf("leitura:%c\n", estados[i].leitura);
 		printf("direcao:%d\n",estados[i].direcao );
 		printf("escrita:%c\n", estados[i].escrita);
@@ -100,7 +79,7 @@ void PrintaVetorEstado(estado *estados, int tam)
 
 
 
-
+//printa a string na forma pedida
 void printPos (char s[], int pos, int num){
 	int i;
 	
@@ -115,6 +94,8 @@ void printPos (char s[], int pos, int num){
 	printf("\n");
 }
 
+
+//acresenta as caracteres '*' e '$' no começo e no final respectivamente
 int AdequaString(char s[])
 {
 	int aux = strlen(s);
@@ -130,18 +111,19 @@ int AdequaString(char s[])
 	s[0] = '*';
 }
 
+// processa a string na maquina de turing recursivamente
 void MaquinaDeTuring(int atual, int pos_string, char *s,estado *vetor_estado, int *vetor_finais, int tam_estados, int tam_final)
 {
 	for (int i = 0; i < tam_estados; ++i)
 	{
-		if(vetor_estado[i].num == atual)
+		if(vetor_estado[i].num == atual) // testa se a transição correta
 		{
-			if (vetor_estado[i].leitura == s[pos_string])
+			if (vetor_estado[i].leitura == s[pos_string]) //testa se a letra é a mesma na posição indicada
 			{
-				if (vetor_estado[i].direcao == DIREITA)
+				if (vetor_estado[i].direcao == DIREITA) // testa se o ponteiro deve ir pra direita ou para esquerda
 				{
 					s[pos_string] = vetor_estado[i].escrita;
-					printPos(s,pos_string,atual);
+					printPos(s,pos_string,vetor_estado[i].qual_ir);
 					pos_string++;
 					return MaquinaDeTuring(vetor_estado[i].qual_ir,pos_string,s,vetor_estado,vetor_finais,tam_estados,tam_final);
 				}else
@@ -153,16 +135,7 @@ void MaquinaDeTuring(int atual, int pos_string, char *s,estado *vetor_estado, in
 				}
 			}
 		}
-	}	for (int i = 0; i < tam_final; ++i)
-	{
-			if (atual == vetor_finais[i]){
-			printf("aceita\n");
-			return;
-		}
 	}
-
-	printf("nao aceita\n");
-	return;
 
 }
 
@@ -178,9 +151,9 @@ int main(int argc, char const *argv[])
 	AdequaString(entrada);
 	printPos(entrada,0,0);
 	MaquinaDeTuring(0,1,entrada,Estados,finais,tam_estados,tam_finais);
-	//PrintaVetorEstado(Estados,tam_estados);
+		//PrintaVetorEstado(Estados,tam_estados);
 
 	//printPos("alisson",3,3);
 
 	return 0;
-}
+}	
